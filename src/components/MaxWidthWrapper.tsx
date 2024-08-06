f7b408ccffa206089444bcddb8608bcd5fab4ccf
Colin Matteson <seefm@proton.me>
2024-08-06 00:21:52 -0400

    more setup

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -9,7 +9,7 @@ const MaxWidthWrapper = ({
     children: ReactNode;
 }) => {
     return (
-        <div className={cn('mx-auto bg-red-500 bg-green-500 w-full max-w-screen-x1 px-2.5 md:px-20', className)}>
+        <div className={cn('mx-auto w-full max-w-screen-x1 px-2.5 md:px-20', className)}>
             {children}
         </div>
     )
